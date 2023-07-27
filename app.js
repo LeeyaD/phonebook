236a8f01d82290deccafe9f9323baf7ddaeb81ba
Leeya Davis <16376036+LeeyaD@users.noreply.github.com>
2023-07-27 09:40:02 -0400

    imported cypress & E2E testing

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/persons', middleware.userExtractor, peopleRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
